515954bfad75e1e9a6039bd82ec6ee7570c2c309
Brian Thomas <Brianthomas571@gmail.com>
2019-01-24 21:43:07 -0600

    changing database url to mongodb uri

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/sports-shore";
+  process.env.MONGODB_URI || "mongodb://localhost/sports-shore";
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/test-sports-shore-app";
 exports.PORT = process.env.PORT || 8080;
